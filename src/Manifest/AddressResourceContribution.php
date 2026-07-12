d21506c3fa80e8042dcdf36c4689cc739e2d67b4
Ben <howdu@users.noreply.github.com>
2026-07-12 08:56:25 +0100

    fix: align packages with Capell v4 extension API

--- src/Manifest/AddressResourceContribution.php
+++ src/Manifest/AddressResourceContribution.php
@@ -11,6 +11,6 @@ final class AddressResourceContribution implements ExtensionContribution, Regist
 {
     public static function compatibleCapellApiVersion(): string
     {
-        return '^0.0';
+        return '^4.0';
     }
 }
