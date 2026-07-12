ef1cab1016e8f008541f31f16c814412d8c966fd
Ben <howdu@users.noreply.github.com>
2026-07-12 07:44:22 +0100

    fix: align packages with 0.0 release line

--- src/Manifest/AddressResourceContribution.php
+++ src/Manifest/AddressResourceContribution.php
@@ -11,6 +11,6 @@ final class AddressResourceContribution implements ExtensionContribution, Regist
 {
     public static function compatibleCapellApiVersion(): string
     {
-        return '^4.0';
+        return '^0.0';
     }
 }
